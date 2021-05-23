dd45bd1e8eb0da709bc5fed494778cd18cd4c459
Subodh Dahal <contact.subodhdahal@gmail.com>
2021-05-23 16:35:48 +0200

    Initial commit on NuxtJS setup

---
+++ pages/index.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="container">
+    <div>
+      <h1 class="title">
+        Hi! <br>
+        I'm Subodh Dahal
+      </h1>
+      <h2 class="subtitle">
+        Software Engineer, Thinker, Wannabe Philosopher
+      </h2>
+      <div class="links">
+        <a
+          href="https://www.linkedin.com/in/subodhdahal/"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--green"
+        >
+          LinkedIn
+        </a>
+        <a
+          href="https://github.com/SubodhDahal"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--grey"
+        >
+          GitHub
+        </a>
+        <a
+          href="https://stackoverflow.com/users/1083422/sub0dh"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--grey"
+        >
+          StackOverflow
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
+
+<style>
+/* Sample `apply` at-rules with Tailwind CSS
+.container {
+@apply min-h-screen flex justify-center items-center text-center mx-auto;
+}
+*/
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family:
+    'Quicksand',
+    'Source Sans Pro',
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    'Helvetica Neue',
+    Arial,
+    sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
