1c25318820e574b96856a428d4ca48c9e0874373
Subodh Dahal <contact.subodhdahal@gmail.com>
2021-05-23 23:43:56 +0200

    refactor(home): used TailWind utility classes for typography

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,21 @@
 <template>
   <div class="bg-gray-300">
     <div class="container">
-      <div class="flex w-full">
-        <div class="w-3/5 self-center">
-          <h1 class="title">
+      <div class="md:flex w-full mb-4">
+        <div class="w-full md:w-3/5 self-center">
+          <h1 class="text-5xl md:text-7xl mb-7 text-gray-700 leading-snug">
             Hi! <br>
             I'm Subodh Dahal
           </h1>
-          <h2 class="subtitle">
+          <h2 class="text-2xl md:text-5xl text-gray-600 leading-snug">
             Software Engineer, Automation Enthusiast,<br> Hobbyist Photographer & Avid Ponderer
           </h2>
-          <h3 class="mt-2 text-4xl text-gray-600">
+          <h3 class="mt-8 mb-2 text-xl md:text-3xl text-gray-500">
             From 🇳🇵, based in 🇩🇪
           </h3>
           <social-links class="mt-8" />
         </div>
-        <object class="w-2/5" type="image/svg+xml" data="/avatar_animated.svg" />
+        <object class="w-40 md:w-2/5 mx-auto mt-8 md:mt-0" type="image/svg+xml" data="/avatar_animated.svg" />
       </div>
     </div>
   </div>
@@ -28,34 +28,7 @@ export default Vue.extend({})
 </script>
 
 <style>
-/* Sample `apply` at-rules with Tailwind CSS */
 .container {
   @apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 90px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
 }
 </style>
