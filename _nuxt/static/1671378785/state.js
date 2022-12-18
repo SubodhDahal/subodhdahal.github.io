94852eb451d3ac2a8f33f2e0d9579f85c2cde854
SubodhDahal <SubodhDahal@users.noreply.github.com>
2022-12-18 16:20:43 +0000

    deploy: a9188eff40a39e65f72bd6c9d8beab65ac15cea6

--- _nuxt/static/1671378785/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1671378785",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"3b7cf88b"}}}}(null,"\u002F"));
