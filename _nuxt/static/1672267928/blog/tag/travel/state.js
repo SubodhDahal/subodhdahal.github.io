76d101757a003e42992e34bf7575d684ffe3b077
SubodhDahal <SubodhDahal@users.noreply.github.com>
2022-12-28 22:52:28 +0000

    deploy: b442faa2e75c5176aa853376bc12620248350716

---
+++ _nuxt/static/1672267928/blog/tag/travel/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1672267928",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Ftag\u002Ftravel",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"a2b8f112"}}}}(null));
