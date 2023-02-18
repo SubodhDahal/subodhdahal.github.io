8145619ee5ee951af06b0a240dad5a1f34e31a1d
Subodh Dahal <contact.subodhdahal@gmail.com>
2023-02-18 15:11:04 +0100

    feat: add a 404 page

---
+++ components/NotFound.vue
@@ -0,0 +1,21 @@
+<template>
+  <div
+    class="lg:px-24 lg:py-24 md:py-20 md:px-44 px-4 py-24 items-center flex justify-center flex-col-reverse lg:flex-row md:gap-28 gap-16 mb-16"
+  >
+    <div class="w-full xl:w-1/2 relative pb-12 lg:pb-0 prose">
+      <h1 class="my-2 font-bold text-2xl">
+        Oops! You've stumbled into a wormhole in my website's code.
+      </h1>
+      <p class="my-2 mb-8">
+        I am working to decode the anomaly and send you back to your dimension.<br>
+        In the meantime, ponder the possibilities of interdimensional travel.
+      </p>
+      <NuxtLink
+        to="/"
+        class="sm:w-full lg:w-auto my-2 border rounded md py-4 px-8 text-center bg-gray-600 text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-700 focus:ring-opacity-50"
+      >
+        Take me there!
+      </NuxtLink>
+    </div>
+  </div>
+</template>
