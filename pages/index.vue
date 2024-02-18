035379bfe4aecf1c5fc4d257c9047d7eda87122e
Subodh Dahal <contact.subodhdahal@gmail.com>
2024-02-18 17:52:06 +0100

    Update about.md and index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -38,7 +38,21 @@
               <img src="/images/me.jpg" class="max-w-full h-auto" alt="Subodh Dahal" />
             </div>
 
-            <ContentDoc path="about" class="md:w-2/3" />
+            <!-- TODO: fix not reading from markdown file -->
+            <!-- <ContentDoc path="about" class="md:w-2/3" /> -->
+            <div class="md:w-2/3">
+              <div>
+                <p>Hi! I'm Subodh Dahal, an experienced software engineer with a strong passion for technology and a growing interest in Large Language Models (LLMs).</p>
+
+                <p>Originally from Nepal, I've been in Germany since 2016, where I earned a Master's in Information Engineering, specializing in Intelligent Systems and Machine Learning.</p>
+
+                <p>Over a decade, I've become skilled in various programming languages like Python, Javascript, Typescript, NodeJS, and PHP. I have a successful track record of delivering innovative solutions.</p>
+
+                <p>As a team lead and mentor, I prioritize effective communication and teamwork for success. I'm dedicated to making technology accessible and supporting the open-source community.</p>
+
+                <p>In my spare time, I enjoy exploring nature, photography, and diving into science fiction for inspiration.</p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
