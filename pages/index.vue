b050e775fcde9b60be18e3ba8637d047b756da99
Subodh Dahal <contact.subodhdahal@gmail.com>
2024-02-18 18:14:46 +0100

    Refactor homepage header layout and styling

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,17 @@
 <template>
   <div>
-    <div class="bg-gray-100 dark:bg-gray-800">
-      <div class="container py-2 md:py-0">
-        <div class="md:flex-row-reverse md:flex w-full pt-2 md:pt-10 md:pt-20 mb-4">
-          <object class="w-40 md:w-2/5 mx-auto md:mt-0" type="image/svg+xml" data="/avatar_animated.svg" />
+    <div class="bg-gray-100 dark:bg-gray-800" id="homepage-header">
+      <div class="container md:py-0">
+        <div class="md:flex-row-reverse md:flex w-full pt-2 md:pt-40 mb-4 justify-end items-end px-8 md:px-8">
           <div class="w-full md:w-3/5 self-center dark:text-gray-50">
-            <h1 class="heading leading-snug">
+            <h1 class="heading leading-snug pb-4">
               Hi! <br>
               I'm <NuxtLink to="/#about" class="hover-underline-animation">Subodh Dahal</NuxtLink>
             </h1>
-            <h2 class="text-2xl md:text-4xl text-gray-600 dark:text-gray-50 leading-snug">
+            <h2 class="text-2xl md:text-3xl text-gray-600 dark:text-gray-50">
               Software Engineer, Automation Enthusiast,<br> Hobbyist Photographer &amp; Avid Ponderer
             </h2>
-            <h3 class="mt-8 mb-2 text-xl md:text-3xl text-gray-500 dark:text-gray-50">
+            <h3 class="mt-8 mb-10 text-xl md:text-3xl text-gray-500 dark:text-gray-50">
               From 🇳🇵, based in 🇩🇪
             </h3>
           </div>
@@ -81,5 +80,11 @@ useHead({
 .heading {
   font-family: 'Source Serif Pro', serif;
   @apply text-5xl md:text-7xl mb-7 text-gray-700 dark:text-gray-50;
+}
+#homepage-header h1 {
+  line-height: 5rem;
+}
+#homepage-header h2 {
+  line-height: 2.6rem;
 }
 </style>
