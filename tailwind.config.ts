d62c97152e20ddf3e20add5ffacc1e23166501d9
Subodh Dahal <contact.subodhdahal@gmail.com>
2025-05-24 09:57:10 +0200

    chore: update styles and typography settings in CSS and Tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from 'tailwindcss'
+import defaultTheme from 'tailwindcss/defaultTheme'
 
 export default <Partial<Config>>{
   content: [
@@ -22,6 +23,23 @@ export default <Partial<Config>>{
           css: {
             'code::before': { content: '' },
             'code::after': { content: '' },
+            h1: {
+              fontFamily: 'Source Serif Pro, serif',
+              fontWeight: '700',
+              marginBottom: '1.5rem'
+            },
+            h2: {
+              fontFamily: 'Source Serif Pro, serif',
+              fontWeight: '700',
+              marginTop: '2rem',
+              marginBottom: '1rem'
+            },
+            a: {
+              textDecoration: 'none',
+              '&:hover': {
+                textDecoration: 'underline'
+              }
+            }
           },
         },
       },
@@ -31,6 +49,21 @@ export default <Partial<Config>>{
         dark: '#091a28',
         elevated: '#dfe8ef',
         linkExactActiveClass: '#fd213b',
+        blog: {
+          text: {
+            light: '#374151', // gray-700
+            dark: '#d1d5db'   // gray-300
+          },
+          bg: {
+            light: '#ffffff',
+            dark: '#111827'   // gray-900
+          }
+        }
+      },
+      fontFamily: {
+        sans: ['Lato', ...defaultTheme.fontFamily.sans],
+        serif: ['Source Serif Pro', ...defaultTheme.fontFamily.serif],
+        display: ['Samble Tracie', 'cursive']
       },
       margin: {
         'top-bar': '100px',
@@ -38,8 +71,9 @@ export default <Partial<Config>>{
     },
   },
   variants: {
-    backgroundColor: [
-    ],
+    extend: {
+      typography: ['dark']
+    }
   },
   plugins: [
     require('@tailwindcss/typography'),
