a8a004c7f1f3624cebd0ce63bec135d0ceb2620c
Subodh Dahal <contact.subodhdahal@gmail.com>
2025-08-25 10:46:39 +0200

    feat: implement rss and atom feeds

--- app.vue
+++ app.vue
@@ -1,21 +1,36 @@
 <script setup>
-const description = 'Subodh Dahal -  Software Engineer, Automation Enthusiast, Hobbyist Photographer & Avid Ponderer'
-const ogTitle = 'Subodh Dahal\'s musings on software engineering, automation, photography and more'
-const ogImage = 'https://subodhdahal.com/twitter-card.png'
-const mySite = 'https://subodhdahal.com'
+const description =
+  "Subodh Dahal -  Software Engineer, Automation Enthusiast, Hobbyist Photographer & Avid Ponderer";
+const ogTitle =
+  "Subodh Dahal's musings on software engineering, automation, photography and more";
+const ogImage = "https://subodhdahal.com/twitter-card.png";
+const mySite = "https://subodhdahal.com";
 
 useHead({
   htmlAttrs: {
-    lang: 'en',
+    lang: "en",
   },
-  charset: 'utf-8',
+  charset: "utf-8",
   title: ogTitle,
-  titleTemplate: title => title !== ogTitle ? `${title} · Subodh Dahal` : title,
+  titleTemplate: (title) =>
+    title !== ogTitle ? `${title} · Subodh Dahal` : title,
   link: [
-    { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-    { rel: 'canonical', href: mySite },
+    { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+    { rel: "canonical", href: mySite },
+    {
+      rel: "alternate",
+      type: "application/rss+xml",
+      title: "Subodh Dahal (RSS)",
+      href: "/rss.xml",
+    },
+    {
+      rel: "alternate",
+      type: "application/atom+xml",
+      title: "Subodh Dahal (Atom)",
+      href: "/atom.xml",
+    },
   ],
-})
+});
 
 useSeoMeta({
   title: ogTitle,
@@ -24,31 +39,31 @@ useSeoMeta({
   ogDescription: description,
   ogImage: ogImage,
   ogUrl: mySite,
-  ogType: 'website',
-  twitterCard: 'summary_large_image',
+  ogType: "website",
+  twitterCard: "summary_large_image",
   twitterTitle: ogTitle,
   twitterDescription: description,
   twitterImage: ogImage,
-})
+});
 
 useSchemaOrg({
   website: {
-    name: 'Subodh Dahal',
+    name: "Subodh Dahal",
     url: mySite,
     description: description,
     image: ogImage,
   },
   person: {
-    name: 'Subodh Dahal',
+    name: "Subodh Dahal",
     url: mySite,
-    image: '/images/me.jpg',
-    jobTitle: 'Software Engineer',
+    image: "/images/me.jpg",
+    jobTitle: "Software Engineer",
     sameAs: [
-      'https://github.com/subodhdahal',
-      'https://linkedin.com/in/subodhdahal',
+      "https://github.com/subodhdahal",
+      "https://linkedin.com/in/subodhdahal",
     ],
   },
-})
+});
 </script>
 
 <template>
